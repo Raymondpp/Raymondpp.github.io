fc46f061bcf7119302c6c1e57ebea461d3cd1c01
Raymondpp <37254576+Raymondpp@users.noreply.github.com>
2020-02-07 00:25:38 +1300

    changed height

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -5,7 +5,7 @@ class Landing extends Component {
     render() {
 
         return (
-            <div style={{ width: '100%', margin: 'auto' }}>
+            <div style={{ height: '100%', width: '100%', margin: 'auto' }}>
                 <Grid className="landing-grid">
                     <Cell col={12}>
                         <img
